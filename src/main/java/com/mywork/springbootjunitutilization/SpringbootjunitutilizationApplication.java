cf56be416f1b266a72bcb09632a52ebaf790eb66
Kavyashree HM <123004602+kavyashreehm08@users.noreply.github.com>
2024-05-04 15:35:41 -0700

    creatting new JUnit project

---
+++ src/main/java/com/mywork/springbootjunitutilization/SpringbootjunitutilizationApplication.java
@@ -0,0 +1,13 @@
+package com.mywork.springbootjunitutilization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjunitutilizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootjunitutilizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mywork/springbootjunitutilization/SpringbootjunitutilizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mywork.springbootjunitutilization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjunitutilizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
